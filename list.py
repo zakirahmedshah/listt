37fc664d1faba931c34a1a18aeb9911b683b4cb3
zakirahmedshah <119791518+zakirahmedshah@users.noreply.github.com>
2024-03-12 03:16:53 -0700

    Add files via upload

---
+++ list.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Define two lists
+list1 = [1, 2, 3, 4, 5]
+list2 = [6, 7, 8, 9, 10]
+
+# Addition
+addition_result = [a + b for a, b in zip(list1, list2)]
+print("Addition Result:", addition_result)
+
+# Subtraction
+subtraction_result = [a - b for a, b in zip(list1, list2)]
+print("Subtraction Result:", subtraction_result)
+
+# Multiplication
+multiplication_result = [a * b for a, b in zip(list1, list2)]
+print("Multiplication Result:", multiplication_result)
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
